9f3fb849d9d4bd24ff8074a2738995becc379870
ubombar <ufukbombar@gmail.com>
2023-02-21 00:26:18 +0100

    Add experimenter

---
+++ experiment.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+import pandas as pd 
+from dataclasses import dataclass
+
+@dataclass
+class ExperimentRound():
+    # First time the experiment started
+    experiment_start_date: datetime
+
+    # The time experiment finished
+    experiment_finsh_date: datetime
+
+    # When the deployments becomes ready
+    experiment_deployment_becoma_ready_date: datetime
+
+    # Start and finish dates of deployment creation
+    experiment_deployment_creation_started_date: datetime
+    experiment_deployment_creation_finished_date: datetime
+
+    # Start and finish dates of deplotment deletion
+    experiment_deployment_deletion_started_date: datetime
+    experiment_deployment_deletion_finished_date: datetime
+
+    # Number of replicas
+    replicas: int
+
+    # Name of the federation framework
+    framework: str 
+
+    # If any error occured
+    error_occured: bool
+
+    # Kubernetes API exception (string representation)
+    exception: str
+
+    # Experiment's current step
+    step: int
+
+    # Total steps for the experiment
+    total_steps: int
+
+    # Time waited in seconds after deployment created
+    cool_period: float
---
+++ experimenter_knative.py
@@ -0,0 +1,202 @@
+from kubernetes import client, config, watch
+from datetime import datetime
+from experiment import ExperimentRound
+import time 
+
+class KnativeExperimenter():
+    def __init__(self, namespace: str, replicas: int, container_image: str, framework: str, cooldown: float) -> None:
+        self.namespace = namespace
+        self.replicas = replicas
+        self.container_image = container_image
+        self.framework = framework
+        self.cooldown = cooldown
+
+        self.deployment_name = "test-deployment"
+        self.container_name = "test-container"
+
+    def run(self, steps):
+        self.load_kube_config()
+        self.check_namespace()
+        experiment_list = []
+
+        estimated_time = int(steps * (1.5 * self.replicas + self.cooldown))
+        print(f"Experiment of {steps} step(s) with {self.replicas} replica(s) in '{self.framework}' and '{self.container_image}' image. Estimated time for the experiment: {estimated_time} second(s)")
+
+        for s in range(steps):
+            eess = []
+            experiment_start_date = datetime.now()
+            experiment_deployment_creation_started_date = experiment_start_date
+            eess.append(self.create_deployment())
+            experiment_deployment_creation_finished_date = datetime.now()
+
+            eess.append(self.wait_until_deployment_ready())
+
+            experiment_deployment_becoma_ready_date = datetime.now()
+
+            time.sleep(self.cooldown)
+
+            experiment_deployment_deletion_started_date = datetime.now()
+            eess.append(self.delete_deployment())
+            experiment_deployment_deletion_finished_date = datetime.now()
+            experiment_finsh_date = experiment_deployment_deletion_finished_date
+
+            exception_occured = eess == [None, None, None]
+
+            experiment_list.append(ExperimentRound(
+                experiment_start_date=experiment_start_date,
+                experiment_finsh_date=experiment_finsh_date,
+
+                experiment_deployment_becoma_ready_date=experiment_deployment_becoma_ready_date,
+                
+                experiment_deployment_creation_started_date=experiment_deployment_creation_started_date,
+                experiment_deployment_creation_finished_date=experiment_deployment_creation_finished_date,
+
+                experiment_deployment_deletion_started_date=experiment_deployment_deletion_started_date,
+                experiment_deployment_deletion_finished_date=experiment_deployment_deletion_finished_date,
+
+                cool_period=self.cooldown,
+                framework=self.framework,
+                replicas=self.replicas,
+                step=s,
+                total_steps=steps,
+                error_occured=exception_occured,
+                exception=str(eess),
+            ))
+
+        return experiment_list
+
+    def load_kube_config(self):
+        config.load_kube_config()
+
+    def check_namespace(self):
+        '''
+            Checks if the given namespace exists, if not creates the namespace. Returns an exception
+            if an error occurs. Go style.
+
+            namespace: Non-empty string name of the namespace.
+        '''
+        v1 = client.CoreV1Api()
+
+        try:
+            namespace_list = [ns.metadata.name for ns in v1.list_namespace().items]
+        except client.ApiException as e:
+            return e 
+
+        if self.namespace in namespace_list:
+            return
+
+        try:
+            v1.create_namespace(client.V1Namespace(
+                metadata=client.V1ObjectMeta(
+                    name=self.namespace
+                )
+            ))
+        except client.ApiException as e:
+            return e
+
+    def create_deployment(self):
+        '''
+            Creates a basic deployment with specified attributes.
+        '''
+        appsv1 = client.AppsV1Api()
+
+        deployment = client.V1Deployment(
+            # api_version="apps/v1",
+            # kind="Deployment",
+            metadata=client.V1ObjectMeta(
+                name=self.deployment_name,
+                labels={
+                    'app': 'test'
+                },
+            ),
+            spec=client.V1DeploymentSpec(
+                replicas=self.replicas,
+                selector=client.V1LabelSelector(
+                    match_labels={
+                        'app': 'test',
+                    },
+                ),
+                template=client.V1PodTemplateSpec(
+                    metadata=client.V1ObjectMeta(
+                        labels={
+                            'app': 'test',
+                        },
+                    ),
+                    spec=client.V1PodSpec(
+                        containers=[
+                            client.V1Container(
+                                name=self.container_name,
+                                image=self.container_image
+                            )
+                        ]
+                    )
+                )
+            ))
+
+        try:
+            appsv1.create_namespaced_deployment(namespace=self.namespace, body=deployment)
+        except client.ApiException as e:
+            return e
+
+    def delete_deployment(self):
+        appsv1 = client.AppsV1Api()
+
+        try:
+            appsv1.delete_namespaced_deployment(name=self.deployment_name, namespace=self.namespace)
+        except client.ApiException as e:
+            return e
+
+    def wait_until_deployment_ready(self, timeout: int | None =None):
+        '''
+            Waits until the deployment is ready. Return and exception if timeout occur. 
+            Default timeout is none.
+        '''
+        w = watch.Watch()
+        appsv1 = client.AppsV1Api()
+
+        try:
+            for event in w.stream(appsv1.list_namespaced_deployment, namespace=self.namespace, _request_timeout=timeout):
+                deployment = event['object'].to_dict()
+
+                if deployment['metadata']['name'] != self.deployment_name: continue
+                
+                ready_replicas = deployment['status']['ready_replicas']
+                replicas = deployment['status']['replicas']
+
+                # If null continue
+                if not ready_replicas: continue
+
+                if ready_replicas == replicas:
+                    w.stop()
+
+        except client.ApiException as e:
+            return e
+        except KeyboardInterrupt:
+            return
+
+# # Quick experiment prepared for KNative environment.
+# # Creates a ns and a deployment. Waits for it ti be created, then immidiately deletes it.
+# if __name__ == "__main__":
+#     k = KnativeExperimenter(namespace="experiments", container_image="nginx", replicas=1)
+#     # config.load_kube_config()
+#     k.load_kube_config()
+#     print("kubeconfig loaded.")
+
+#     print("Checking namespace")
+#     g = k.check_namespace()
+
+#     print(g)
+
+#     print("Creating deployment")
+#     k.create_deployment()
+
+#     print("Waiting...")
+#     k.wait_until_deployment_ready()
+
+#     print("Deleting deployment")
+#     k.delete_deployment()
+
+#     print("Experiment coldstart done!")
+
+k = KnativeExperimenter(namespace="experiments", container_image="nginx", replicas=1, framework="knative", cooldown=10)
+print(k.run(1))
