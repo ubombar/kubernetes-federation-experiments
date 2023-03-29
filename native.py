6d705a9f22185a6492c6ce08c944330c51ed31bf
Your Name <you@example.com>
2023-03-29 03:50:03 -0600

    Added kubeconfig file option to native experiment

--- native.py
+++ native.py
@@ -16,30 +16,30 @@ import json
 from tqdm import tqdm
 import itertools
 
-def native_deployment_type_experiment(pod_count, namespace, first_sleep_seconds, second_sleep_seconds) -> dict:
+def native_deployment_type_experiment(pod_count, namespace, first_sleep_seconds, second_sleep_seconds, kubeconfig_file_native) -> dict:
     config.load_kube_config()
     result = {
         "deployment_events": list(),
         "pod_events": collections.defaultdict(list),
     }
 
-    deployment_thread = threading.Thread(target=retrieve_deployment_events, args=[namespace, "deployment", result["deployment_events"]])
+    deployment_thread = threading.Thread(target=retrieve_deployment_events, args=[kubeconfig_file_native, namespace, "deployment", result["deployment_events"]])
     deployment_thread.start()
 
-    pods_thread = threading.Thread(target=retrieve_pods_events, args=[namespace, result["pod_events"]])
+    pods_thread = threading.Thread(target=retrieve_pods_events, args=[kubeconfig_file_native, namespace, result["pod_events"]])
     pods_thread.start()
 
     result["time_before_create"] = str(datetime.now())
-    util_create_deployment(namespace, "deployment", pod_count)
+    util_create_deployment(kubeconfig_file_native, namespace, "deployment", pod_count)
 
     result["time_before_wait"] = str(datetime.now())
-    util_wait_until_deployment_ready(namespace, "deployment")
+    util_wait_until_deployment_ready(kubeconfig_file_native, namespace, "deployment")
 
     result["time_before_sleep1"] = str(datetime.now())
     time.sleep(first_sleep_seconds)
 
     result["time_before_delete"] = str(datetime.now())
-    util_delete_deployment(namespace, "deployment")
+    util_delete_deployment(kubeconfig_file_native, namespace, "deployment")
     
     result["time_before_sleep2"] = str(datetime.now())
     time.sleep(second_sleep_seconds)
@@ -51,30 +51,30 @@ def native_deployment_type_experiment(pod_count, namespace, first_sleep_seconds,
     result["time_before_end"] = str(datetime.now())
     return result
 
-def native_deployments_type_experiment(pod_count, namespace, first_sleep_seconds, second_sleep_seconds) -> dict:
+def native_deployments_type_experiment(pod_count, namespace, first_sleep_seconds, second_sleep_seconds, kubeconfig_file_native) -> dict:
     config.load_kube_config()
     result = {
         "deployment_events": collections.defaultdict(list),
         "pod_events": collections.defaultdict(list),
     }
 
-    deployments_thread = threading.Thread(target=retrieve_deployments_events, args=[namespace, result["deployment_events"]])
+    deployments_thread = threading.Thread(target=retrieve_deployments_events, args=[kubeconfig_file_native, namespace, result["deployment_events"]])
     deployments_thread.start()
     
-    pods_thread = threading.Thread(target=retrieve_pods_events, args=[namespace, result["pod_events"]])
+    pods_thread = threading.Thread(target=retrieve_pods_events, args=[kubeconfig_file_native, namespace, result["pod_events"]])
     pods_thread.start()
 
     result["time_before_create"] = str(datetime.now())
-    util_create_deployments(namespace, pod_count)
+    util_create_deployments(kubeconfig_file_native, namespace, pod_count)
 
     result["time_before_wait"] = str(datetime.now())
-    util_wait_until_deployments_ready(namespace, pod_count)
+    util_wait_until_deployments_ready(kubeconfig_file_native, namespace, pod_count)
 
     result["time_before_sleep1"] = str(datetime.now())
     time.sleep(first_sleep_seconds)
 
     result["time_before_delete"] = str(datetime.now())
-    util_delete_deployments(namespace, pod_count)
+    util_delete_deployments(kubeconfig_file_native, namespace, pod_count)
     
     result["time_before_sleep2"] = str(datetime.now())
     time.sleep(second_sleep_seconds)
@@ -90,12 +90,15 @@ def experiment_deployments(pod_counts: list[int], single_deployment: bool, num_i
     experiments = []
     filepath = os.path.join("jsons", generate_name())
 
+    # Info
+    kubeconfig_file_native = "~/.kube/config"
+
     for pod_count, iteration in tqdm(itertools.product(pod_counts, range(num_iterations)), total=len(pod_counts)*num_iterations):
         data = None
         if single_deployment:
-            data = native_deployment_type_experiment(pod_count, namespace, first_sleep, second_sleep)
+            data = native_deployment_type_experiment(pod_count, namespace, first_sleep, second_sleep, kubeconfig_file_native)
         else:
-            data = native_deployments_type_experiment(pod_count, namespace, first_sleep, second_sleep)
+            data = native_deployments_type_experiment(pod_count, namespace, first_sleep, second_sleep, kubeconfig_file_native)
 
         experiment = {
             "data": data,
